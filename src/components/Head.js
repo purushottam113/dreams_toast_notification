a16f7499b02208c2a8871c17f93faee99c6b3ba2
purushottam113 <shreepad113@gmail.com>
2024-04-21 12:09:20 +0530

    fixed UI bug

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Head = () => {
   return (
-    <div className='bg-red-500 flex content-between justify-between h-16 px-2 text-white'>
-      <h1 className='text-2xl my-auto'>Heading</h1>
+    <div className='bg-red-500 flex content-between justify-between h-16 px-2 text-white w-screen'>
+      <h1 className='sm:text-2xl text-xl'>Dreams</h1>
       <div className="flex gap-2 content-center">
-      <Link to="/"><h1 className='hover:bg-red-600 px-2 m-2 rounded-sm'>First Component</h1></Link>
-      <Link to="/component2"><h1 className='hover:bg-red-600 px-2 m-2 rounded-sm'>Second Component</h1></Link>
-      <Link to="/component3"><h1 className='hover:bg-red-600 px-2 m-2 rounded-sm'>Third Component</h1></Link>
+      <Link to="/"><h1 className='hover:bg-red-600 px-2 m-2 rounded-sm text-sm sm:text-lg'>First Component</h1></Link>
+      <Link to="/component2"><h1 className='hover:bg-red-600 px-2 m-2 rounded-sm text-sm sm:text-lg'>Second Component</h1></Link>
+      <Link to="/component3"><h1 className='hover:bg-red-600 px-2 m-2 rounded-sm text-sm sm:text-lg'>Third Component</h1></Link>
       </div>
     </div>
   )
